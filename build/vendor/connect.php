afc68bb68d8244dbb79fe9fe0a629d062a7c8771
VsevolodKurochka <dramdoobas123456@gmail.com>
2018-09-17 19:32:13 +0300

    Fix

--- build/vendor/connect.php
+++ build/vendor/connect.php
@@ -1,8 +1,10 @@
 <?php
+	
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+
 	require_once 'autoload.php';
 	$loader = new Twig_Loader_Filesystem('views');
-	$twig = new Twig_Environment($loader, array(
-		'debug'	=> true
-	));
-	$twig->addExtension(new Twig_Extension_Debug());
+	$twig = new Twig_Environment($loader);
 ?>
